e6c2a304fbbf723ed8d0a966349ff8d6c6863f03
JorgeQZ <jorge_qzg@hotmail.com>
2022-07-28 17:21:59 -0500

    mobile frontpage

--- archive-gaceta.php
+++ archive-gaceta.php
@@ -16,80 +16,81 @@ get_header();
 <div class="container">
     <div class="grid-list-item">
 
+
         <!-- item -->
-        <a href="http://localhost:8888/ipa/gaceta/mayo-2022/" class="item" style="background-image: url(<?php echo get_template_directory_uri(  ).'/img/close-plant-female-hands-care-600w-1672039984.jpg-copy-31.png' ?>);">
+        <a href="    <?php echo get_home_url().'/gaceta/mayo-2022/';?>" class="item" style="background-image: url(<?php echo get_template_directory_uri(  ).'/img/close-plant-female-hands-care-600w-1672039984.jpg-copy-31.png' ?>);">
             <div class="title">title</div>
             <div class="desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem assumenda illum accusantium?</div>
             <div class="overlay"></div>
         </a><!-- item -->
         <!-- item -->
-        <a href="http://localhost:8888/ipa/gaceta/mayo-2022/" class="item" style="background-image: url(<?php echo get_template_directory_uri(  ).'/img/close-plant-female-hands-care-600w-1672039984.jpg-copy-31.png' ?>);">
+        <a href="    <?php echo get_home_url().'/gaceta/mayo-2022/';?>" class="item" style="background-image: url(<?php echo get_template_directory_uri(  ).'/img/close-plant-female-hands-care-600w-1672039984.jpg-copy-31.png' ?>);">
             <div class="title">title</div>
             <div class="desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem assumenda illum accusantium?</div>
             <div class="overlay"></div>
         </a><!-- item -->
         <!-- item -->
-        <a href="http://localhost:8888/ipa/gaceta/mayo-2022/" class="item" style="background-image: url(<?php echo get_template_directory_uri(  ).'/img/close-plant-female-hands-care-600w-1672039984.jpg-copy-31.png' ?>);">
+        <a href="    <?php echo get_home_url().'/gaceta/mayo-2022/';?>" class="item" style="background-image: url(<?php echo get_template_directory_uri(  ).'/img/close-plant-female-hands-care-600w-1672039984.jpg-copy-31.png' ?>);">
             <div class="title">title</div>
             <div class="desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem assumenda illum accusantium?</div>
             <div class="overlay"></div>
         </a><!-- item -->
         <!-- item -->
-        <a href="http://localhost:8888/ipa/gaceta/mayo-2022/" class="item" style="background-image: url(<?php echo get_template_directory_uri(  ).'/img/close-plant-female-hands-care-600w-1672039984.jpg-copy-31.png' ?>);">
+        <a href="    <?php echo get_home_url().'/gaceta/mayo-2022/';?>" class="item" style="background-image: url(<?php echo get_template_directory_uri(  ).'/img/close-plant-female-hands-care-600w-1672039984.jpg-copy-31.png' ?>);">
             <div class="title">title</div>
             <div class="desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem assumenda illum accusantium?</div>
             <div class="overlay"></div>
         </a><!-- item -->
         <!-- item -->
-        <a href="http://localhost:8888/ipa/gaceta/mayo-2022/" class="item" style="background-image: url(<?php echo get_template_directory_uri(  ).'/img/close-plant-female-hands-care-600w-1672039984.jpg-copy-31.png' ?>);">
+        <a href="    <?php echo get_home_url().'/gaceta/mayo-2022/';?>" class="item" style="background-image: url(<?php echo get_template_directory_uri(  ).'/img/close-plant-female-hands-care-600w-1672039984.jpg-copy-31.png' ?>);">
             <div class="title">title</div>
             <div class="desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem assumenda illum accusantium?</div>
             <div class="overlay"></div>
         </a><!-- item -->
         <!-- item -->
-        <a href="http://localhost:8888/ipa/gaceta/mayo-2022/" class="item" style="background-image: url(<?php echo get_template_directory_uri(  ).'/img/close-plant-female-hands-care-600w-1672039984.jpg-copy-31.png' ?>);">
+        <a href="    <?php echo get_home_url().'/gaceta/mayo-2022/';?>" class="item" style="background-image: url(<?php echo get_template_directory_uri(  ).'/img/close-plant-female-hands-care-600w-1672039984.jpg-copy-31.png' ?>);">
             <div class="title">title</div>
             <div class="desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem assumenda illum accusantium?</div>
             <div class="overlay"></div>
         </a><!-- item -->
         <!-- item -->
-        <a href="http://localhost:8888/ipa/gaceta/mayo-2022/" class="item" style="background-image: url(<?php echo get_template_directory_uri(  ).'/img/close-plant-female-hands-care-600w-1672039984.jpg-copy-31.png' ?>);">
+        <a href="    <?php echo get_home_url().'/gaceta/mayo-2022/';?>" class="item" style="background-image: url(<?php echo get_template_directory_uri(  ).'/img/close-plant-female-hands-care-600w-1672039984.jpg-copy-31.png' ?>);">
             <div class="title">title</div>
             <div class="desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem assumenda illum accusantium?</div>
             <div class="overlay"></div>
         </a><!-- item -->
         <!-- item -->
-        <a href="http://localhost:8888/ipa/gaceta/mayo-2022/" class="item" style="background-image: url(<?php echo get_template_directory_uri(  ).'/img/close-plant-female-hands-care-600w-1672039984.jpg-copy-31.png' ?>);">
+        <a href="    <?php echo get_home_url().'/gaceta/mayo-2022/';?>" class="item" style="background-image: url(<?php echo get_template_directory_uri(  ).'/img/close-plant-female-hands-care-600w-1672039984.jpg-copy-31.png' ?>);">
             <div class="title">title</div>
             <div class="desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem assumenda illum accusantium?</div>
             <div class="overlay"></div>
         </a><!-- item -->
         <!-- item -->
-        <a href="http://localhost:8888/ipa/gaceta/mayo-2022/" class="item" style="background-image: url(<?php echo get_template_directory_uri(  ).'/img/close-plant-female-hands-care-600w-1672039984.jpg-copy-31.png' ?>);">
+        <a href="    <?php echo get_home_url().'/gaceta/mayo-2022/';?>" class="item" style="background-image: url(<?php echo get_template_directory_uri(  ).'/img/close-plant-female-hands-care-600w-1672039984.jpg-copy-31.png' ?>);">
             <div class="title">title</div>
             <div class="desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem assumenda illum accusantium?</div>
             <div class="overlay"></div>
         </a><!-- item -->
         <!-- item -->
-        <a href="http://localhost:8888/ipa/gaceta/mayo-2022/" class="item" style="background-image: url(<?php echo get_template_directory_uri(  ).'/img/close-plant-female-hands-care-600w-1672039984.jpg-copy-31.png' ?>);">
+        <a href="    <?php echo get_home_url().'/gaceta/mayo-2022/';?>" class="item" style="background-image: url(<?php echo get_template_directory_uri(  ).'/img/close-plant-female-hands-care-600w-1672039984.jpg-copy-31.png' ?>);">
             <div class="title">title</div>
             <div class="desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem assumenda illum accusantium?</div>
             <div class="overlay"></div>
         </a><!-- item -->
         <!-- item -->
-        <a href="http://localhost:8888/ipa/gaceta/mayo-2022/" class="item" style="background-image: url(<?php echo get_template_directory_uri(  ).'/img/close-plant-female-hands-care-600w-1672039984.jpg-copy-31.png' ?>);">
+        <a href="    <?php echo get_home_url().'/gaceta/mayo-2022/';?>" class="item" style="background-image: url(<?php echo get_template_directory_uri(  ).'/img/close-plant-female-hands-care-600w-1672039984.jpg-copy-31.png' ?>);">
             <div class="title">title</div>
             <div class="desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem assumenda illum accusantium?</div>
             <div class="overlay"></div>
         </a><!-- item -->
         <!-- item -->
-        <a href="http://localhost:8888/ipa/gaceta/mayo-2022/" class="item" style="background-image: url(<?php echo get_template_directory_uri(  ).'/img/close-plant-female-hands-care-600w-1672039984.jpg-copy-31.png' ?>);">
+        <a href="    <?php echo get_home_url().'/gaceta/mayo-2022/';?>" class="item" style="background-image: url(<?php echo get_template_directory_uri(  ).'/img/close-plant-female-hands-care-600w-1672039984.jpg-copy-31.png' ?>);">
             <div class="title">title</div>
             <div class="desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem assumenda illum accusantium?</div>
             <div class="overlay"></div>
         </a><!-- item -->
         <!-- item -->
-        <a href="http://localhost:8888/ipa/gaceta/mayo-2022/" class="item" style="background-image: url(<?php echo get_template_directory_uri(  ).'/img/close-plant-female-hands-care-600w-1672039984.jpg-copy-31.png' ?>);">
+        <a href="    <?php echo get_home_url().'/gaceta/mayo-2022/';?>" class="item" style="background-image: url(<?php echo get_template_directory_uri(  ).'/img/close-plant-female-hands-care-600w-1672039984.jpg-copy-31.png' ?>);">
             <div class="title">title</div>
             <div class="desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem assumenda illum accusantium?</div>
             <div class="overlay"></div>
