8d846c7da598a6892bd7af5245fc7af8589b893f
JorgeQz <jorge_qzg@hotmail.com>
2022-08-12 00:28:40 -0500

    nosotros autoadmin

--- page.php
+++ page.php
@@ -1,11 +1,24 @@
 <?php
 get_header();
 
-if(have_posts()):
-    while(have_posts()):
-        the_post();
-        the_content();
-    endwhile;
-endif;
-get_footer();
-?>
+$ft_image = wp_get_attachment_url( get_post_thumbnail_id($post->ID));
+if($ft_image):
+    ?>
+    <div class="banner-cont" style="background-image: url(<?php echo $ft_image ?>);">
+        <div class="title-banner">
+            <?php the_title(); ?>
+        </div>
+    </div>
+    <?php
+endif; ?>
+<div class="main-content">
+    <?php
+    if(have_posts()):
+        while(have_posts()):
+            the_post();
+            the_content();
+        endwhile;
+    endif;
+    ?>
+</div>
+<?php get_footer(); ?>
